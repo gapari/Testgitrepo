36c74713bd3bfb8c1b4535a8fb6ba033ced28232
Paripelly Gangadhargit config --global user.email v-gapari@microsoft.comgit config --global user.name Paripelly <you@example.com>
2022-11-15 22:52:55 +0530

    arthematic program

--- Test.py
+++ Test.py
@@ -1,1 +1,12 @@
-print ("I am learning Git course")
+#Arthematic Operator
+
+a=20
+b=10
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+from datetime import date
