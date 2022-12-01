b98c32b7c1f0cf1e62e5f33f3cd15d07481a4c04
Paripelly Gangadhargit config --global user.email v-gapari@microsoft.comgit config --global user.name Paripelly <you@example.com>
2022-11-30 23:46:50 -0800

    substraction

--- newfile.py
+++ newfile.py
@@ -4,11 +4,3 @@ b = 20
 c = a+b
 #perform subtraction of a and b
 d = a-b
-#perform multiplication of a and b
-e = a*b
-#perform division of a and b
-f = a/b
-print 'The addition of a and b is: ',c
-print 'The subtraction of a and b is: ',d
-print 'The multiplication of a and b is: ',e
-
